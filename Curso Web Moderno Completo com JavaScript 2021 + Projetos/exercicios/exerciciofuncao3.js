b4b14029788234d1bdc47e3c01c9114526286207
Jorge Martins <jorgepmbr@gmail.com>
2021-08-20 19:12:52 -0300

    Create exerciciofuncao3.js
    Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário
    num mês, e o quanto ele recebe por hora. A função deverá calcular o salário líquido mensal do funcionário, que é
    da quantidade de horas trabalhadas no mês multiplicada pelo valor da sua hora. Desse valor, deve ser subtraído
    30%, relativo a impostosO retorno da função deve ser a string "Salário líquido é igual a R$ X", em que X é o salário líquido do funcionário
    no mês.

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/exerciciofuncao3.js
@@ -0,0 +1,10 @@
+const horasTrabalhadas = (mes, hora) => {
+    const salarioBruto =  mes  * hora
+    var salarioLiquido = salarioBruto - salarioBruto * 30/100
+    this.salarioLiquido
+
+    return `Salario igual a R$ ${salarioLiquido}`
+}
+
+
+
