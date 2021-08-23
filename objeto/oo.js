69f3caa302a9c4edf80d69074d6a20e47324b307
jorginskn <jorgepmbr@gmail.com>
2021-08-23 17:09:32 -0300

    Create oo.js

---
+++ objeto/oo.js
@@ -0,0 +1,20 @@
+// CODIGO NÃO EXECUTAVEL!!!
+
+// PROCEDURAL 
+processamento(valor1, valor2, valor3)
+
+// OO 
+    objeto = { 
+        valor1,
+        valor2,
+        valor3,
+        processamento(){
+            //...
+        }
+    }
+
+
+objeto.processamento() //Foco passou a ser o objeto
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento 
