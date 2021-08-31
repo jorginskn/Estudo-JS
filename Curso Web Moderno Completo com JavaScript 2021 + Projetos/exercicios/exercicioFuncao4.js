2da4535532531bb62ce3e977b4e584be8a4f2deb
jorginskn <jorgepmbr@gmail.com>
2021-08-31 00:00:55 -0300

    Create exercicioFuncao4.js

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/exercicioFuncao4.js
@@ -0,0 +1,14 @@
+function maiorOuIgual (n1, n2){
+    
+ if (n1 == n2){
+     console.log(true)
+ }  else if (n1 > n2 || n2 > n1)(
+     console.log(false)
+ )}
+
+    
+
+
+maiorOuIgual(0, 0)
+maiorOuIgual(5, 1)
+maiorOuIgual(1, 5)
