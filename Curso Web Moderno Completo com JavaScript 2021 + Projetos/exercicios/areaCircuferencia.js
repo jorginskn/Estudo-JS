e390ed578e70af6e8141920c1bdf3d458fd817ba
jorginskn <jorgepmbr@gmail.com>
2021-08-30 23:41:22 -0300

    Treinamento JS
    exe

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/areaCircuferencia.js
@@ -0,0 +1,6 @@
+const PI = 3.14 
+let r = 5.16 
+areaDeCircuferencia = PI * (r * r)
+console.log(areaDeCircuferencia)
+
+//Área de circuferencia = PI * R2
---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/onibus.js
@@ -0,0 +1,12 @@
+/*const cozinharPão = function (true, false){
+ let fornoAceso 
+    
+    if(fornoAceso = false){
+    console.log("botar lenha")
+    }else {
+        console.log("acender fogo")
+    }
+}
+
+
+cozinharPão(false)
