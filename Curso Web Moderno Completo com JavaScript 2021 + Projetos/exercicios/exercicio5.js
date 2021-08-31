df6fc3d46db9265f19fc4f9e8f092a09c3bfdb03
jorginskn <jorgepmbr@gmai.com>
2021-08-31 17:15:56 -0300

    Treinamento JS
    /*Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/exercicio5.js
@@ -0,0 +1,19 @@
+/*Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a
+multiplicação deles. Porém, não utilize o operador de mutiplicação.*/
+const multiplicar = function (a, b){
+
+    return a * b
+}
+
+
+console.log(multiplicar(5, 5))
+
+
+
+function mult (n1, n2){
+
+    return console.log(n1 * n2)
+
+}
+
+mult(10, 10)
--- Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/exercicioFuncao4.js
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/exercicios/exercicioFuncao4.js
@@ -1,3 +1,5 @@
+//Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+
 function maiorOuIgual (n1, n2){
     
  if (n1 == n2){
