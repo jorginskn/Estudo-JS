4ef4b672f30fca8fd7d9a999067695af9b5347ab
Jorge Martins <jorgepmbr@gmail.com>
2021-10-08 17:16:50 -0300

    Aprendendo a passar parametros com NodeJs

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/node/passandoParametrosCliente.js
@@ -0,0 +1,4 @@
+ // constante  chamada saudaçoes que  vai receber o resultado de require  e acessando a função e seus parametros
+const saudacoes = require ('./passandoParametros')('Ana', 'Lucas', 'João')
+console.log(saudacoes)
+
