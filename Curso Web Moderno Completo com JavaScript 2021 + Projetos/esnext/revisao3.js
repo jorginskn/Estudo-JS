35baf013b080f93686997a507bc6c14c8d86d2b7
Jorge Martins <jorgepmbr@gmail.com>
2021-10-13 18:19:14 -0300

    Object  values e entries notação literal e classe

---
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/esnext/revisao3.js
@@ -0,0 +1,23 @@
+//ES8: Object.values me da os valores atributos do objet/Object.Entries
+const obj = {a: 1, b: 2, c: 3,} 
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// Melhorias  na notação literal de objeto 
+const nome = 'Carla'
+const pessoa ={
+    nome,
+    ola(){
+        return 'Oi gente !'
+    }
+}
+console.log(pessoa.nome, pessoa.ola())
+
+//Class 
+class Animal {}
+class Cachorro extends Animal{
+    falar(){
+        return 'Au Au!'
+    }
+}
+console.log(new Cachorro().falar)
