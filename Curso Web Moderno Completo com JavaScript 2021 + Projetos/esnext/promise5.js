e9f0aaba996dc6bf153224bb8309815804830dc4
Jorge Martins <jorgepmbr@gmail.com>
2021-10-26 20:55:16 -0300

    comentarios

--- Curso Web Moderno Completo com JavaScript 2021 + Projetos/esnext/promise5.js
+++ Curso Web Moderno Completo com JavaScript 2021 + Projetos/esnext/promise5.js
@@ -1,9 +1,9 @@
-function funcionarOuNão(valor, chanceErro){
-    return new Promise((resolve,reject)=>{
-        if(Math.random() < chanceErro){
-            reject('Ocorreu um erro!')
-        }else{
-            resolve(valor)
+function funcionarOuNão(valor, chanceErro){// chamei uma função com dois parametros valor e chance de erro 
+    return new Promise((resolve,reject)=>{//essa função vai me tornar uma promise 
+        if(Math.random() < chanceErro){// se um numero aleatorio for menor que a chance de erro 
+            reject('Ocorreu um erro!')// chamei o reject e mandei um recado
+        }else{//caso contrario imprima 
+            resolve(valor)//a variavel valor    
         }
     })
 }
